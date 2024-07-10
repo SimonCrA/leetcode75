809b4d156d901774baa0842f6ad46852dddfc55d
Simon Clemente <csimonr28@gmail.com>
2024-07-10 15:49:58 -0400

    problema corregido

--- 1768/1768.go
+++ 1768/1768.go
@@ -14,15 +14,15 @@ func MergeAlternately(word1 string, word2 string) string {
 
 	var output s.Builder
 
-	var i int
-	var j int
-	for i < len(*word1Ptr) || i < len(*word2Ptr) {
+	i := 0
+	j := 0
+	for i < len(*word1Ptr) || j < len(*word2Ptr) {
 		if i < len(*word1Ptr) {
 			output.WriteByte((*word1Ptr)[i])
 			i++
 		}
 
-		if j < len(*word1Ptr) {
+		if j < len(*word2Ptr) {
 			output.WriteByte((*word2Ptr)[j])
 			j++
 		}
