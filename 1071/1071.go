1e93294ae006225bfcdc27768736d639020ec6f2
simoncra <csimonr28@gmail.com>
2024-07-13 11:39:19 -0400

    ejercicio resuelto

--- 1071/1071.go
+++ 1071/1071.go
@@ -1,5 +1,9 @@
 package gcd_strings
 
+import (
+	"fmt"
+)
+
 //   - Since the candidate string needs to evenly divide str1 and str2 we only need to consider candidate strings
 //     with a length that is evenly divisible into the length of both str1 and str2. This quickly provides us with
 //     a solution that significantly outperforms brute-force.
@@ -9,5 +13,21 @@ package gcd_strings
 //     evenly divides both strings and work downward to smaller length strings. The first string that fits our criteria
 //     is then our answer.
 func GcdOfStrings(str1 string, str2 string) string {
-	return ""
+	if str1+str2 != str2+str1 {
+		return ""
+	}
+
+	outputStr := gcd(len(str1), len(str2))
+	fmt.Println(outputStr)
+
+	return str1[:outputStr]
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
 }
--- main.go
+++ main.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 
 	gcd_strings "github.com/simoncra/leetcode75/1071"
-	merge_string "github.com/simoncra/leetcode75/1768"
 )
 
 func main() {
-	stringMerged := merge_string.MergeAlternately("ab", "pqrs")
+	// stringMerged := merge_string.MergeAlternately("ab", "pqrs")
 	gcd := gcd_strings.GcdOfStrings("ABCABC", "ABC")
 
-	fmt.Println("merge_string", stringMerged)
+	// fmt.Println("merge_string", stringMerged)
 	fmt.Println("gcd_strings", gcd)
 }
